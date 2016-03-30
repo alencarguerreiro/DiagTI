111f32d47c53f0b3136378fcc23a3a3026351219
alencarguerreiro <alencarguerreiro@gmail.com>
2016-03-30 17:27:35 -0300

    DiagTI V1.01
    Versão 1.01 - Datas

---
+++ Default/www/_phoneGapSettings.js
@@ -0,0 +1,49 @@
+function _phoneGapSettings() { 
+ return {
+    "appId": "com.alencar",
+    "preferences": {
+        "permissions": "",
+        "orientation": "portrait",
+        "target-device": "universal",
+        "fullscreen": "true",
+        "webviewbounce": "true",
+        "prerendered-icon": "false",
+        "stay-in-webview": "false",
+        "ios-statusbarstyle": "default",
+        "detect-data-types": "true",
+        "exit-on-suspend": "false",
+        "show-splash-screen-spinner": "true",
+        "auto-hide-splash-screen": "true",
+        "disable-cursor": "false",
+        "android-minSdkVersion": "7",
+        "android-installLocation": "internalOnly",
+        "phonegap-version": "cli-5.2.0",
+        "FadeSplashScreen": "true",
+        "FadeSplashScreenDuration": "2",
+        "EnableViewportScale": "false",
+        "MediaPlaybackRequiresUserAction": "true",
+        "AllowInlineMediaPlayback": "false",
+        "TopActivityIndicator": "gray",
+        "BackupWebStorage": "cloud",
+        "iosPersistentFileLocation": "Library",
+        "KeyboardDisplayRequiresUserAction": "true",
+        "SurpressesIncrementalRendering": "true",
+        "android-maxSdkVersion": "",
+        "android-targetSdkVersion": "",
+        "KeepRunning": "true",
+        "splash-screen-duration": "5000",
+        "ErrorUrl": "error.html",
+        "LoadingDialog": "Please wait, the app is loading.",
+        "LoadingPageDialog": "Please wait, the data is loading.",
+        "LoadUrlTimeoutValue": "20000",
+        "AndroidPersistentFileLocation": "Internal"
+    },
+    "plugins": {
+        "network-information": "npm",
+        "splashscreen": "npm",
+        "de.websector.datepicker": "pgb",
+        "de.appplant.cordova.plugin.local-notification": "pgb",
+        "whitelist": "npm"
+    }
+};
+}
